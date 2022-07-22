ed48e0d3affeb561f41ef81c3ec2829e910e7bd0
Angel Escobar <escobar@Angels-MacBook-Pro-162.local>
2022-07-21 22:56:16 -0500

    data is being output by city

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,18 @@
 var apiKey = '7e461f5d070af7de1c2d5bd578274de9'
 // var searchBox = document.querySelector('#info').value
 var searchButton = document.querySelector('#choose')
-var tempInfo = document.querySelector('#temp')
-var windInfo = document.querySelector('#wind')
-var humidityInfo = document.querySelector('#humidity')
+var cityList = document.querySelector('#city-weather')
+var fiveDays = document.querySelector('#futuro')
+
 
 
 searchButton.addEventListener('click', function(){
     searchVal = document.querySelector("#info").value;
     requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchVal}&appid=7e461f5d070af7de1c2d5bd578274de9`
 
+    
+        
+    
 
     $.ajax({
         url: requestUrl,
@@ -33,17 +36,88 @@ searchButton.addEventListener('click', function(){
             var currentHumid = data.current.humidity + ' %'
             var currentWind = data.current.wind_speed + ' MPH'
             var currentUvi = data.current.uvi 
+            var future = data.daily[0].temp.max
 
             var tempUvi = document.querySelector('#Uvi')
             var tempSpace = document.querySelector('#temp')
             var tempHumid = document.querySelector('#humidity')
             var tempWind = document.querySelector('#wind')
+            var tempFuturo = document.querySelector('#pasttemp')
 
             tempWind.innerHTML = currentWind
             tempHumid.innerHTML = currentHumid
             tempSpace.innerHTML = currentTemp
             tempUvi.innerHTML = currentUvi
+            tempFuturo.innerHTML = future
+
+            var time = moment().format('dddd, MMMM Do YYYY')
+            $('#currents').text(time)
+
+            // var changeDate = moment.unix(future).format('dddd, MMMM, Do YYYY')
+            // console.log(changeDate)
+
+            $.ajax({
+                url: 'https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&exclude=current,minutely,hourly,alerts&appid=7e461f5d070af7de1c2d5bd578274de9&units=imperial',
+                method: 'Get',
+            }).then(function(days) {
+                // console.log(days.daily[i].dt)
+                for (var i = 0; i <= 5; i++) { 
+
+                var conversion = days.data[i].dt;
+                var furtureDay = new Date(conversion*1000);
+                var forecast = document.createElement('ul')
+
+                var dataTemp = days.daily[i].temp.day;
+                var dataWind = days.daily[i].wind_speed;
+                var dataHumid = days.daily[i].humidity
+
+                var tempura = document.createElement('li');
+                var viento = document.createElement('li');
+                var humidEl = document.createElement('li');
+
+                tempura.innerHTML = dataTemp
+                viento.innerText = dataWind
+                humidEl.innerHTML = dataHumid
+
+                forecast.appendChild(tempura)
+                forecast.appendChild(viento)
+                forecast.appendChild(humidEl)
+                fiveDays.appendChild(forecast)
+
+                }
+
+            })
+
+        //     var cities = [];
+
+        //     function renderCities(){
+
+            
+        //     cityList.innerHTML = "";
+
+        //     for(var i=0; i < cities.length; i++) {
+        //         var city = cities[i];
+
+        //         var li = document.createElement('li');
+        //         li.textContent = city;
+
+        //         cityList.appendChild(li);
+        //     }
+        // }
+
+        //     function init() {
+        //         localStorage.setItem('cities', JSON.stringify(cities))
+            
+        //         renderCities()
+            
+        //     }
+
+
+
+
+
         })
+
 
 
     })
@@ -51,4 +125,3 @@ searchButton.addEventListener('click', function(){
 
 })
         
-  
